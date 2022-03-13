1f3b9526972fe6b6e5b4ab0bbb0b58b0bdfab397
Naash <dgmg@outlook.fr>
2022-03-13 18:41:58 +0100

    add CSS in webpack + responsive website

--- src/Controller/RechercheController.php
+++ src/Controller/RechercheController.php
@@ -13,11 +13,12 @@ use App\Service\CallApiService;
 #[Route('/recherche', name: 'recherche')]
 class RechercheController extends AbstractController
 {
-    #[Route('/', name: 'byparam')]
+    #[Route( name: 'byparam')]
     public function index(Request $request, CallApiService $callApiService): Response
     {
+        // return $this->redirectToRoute("index");
         $response = null;
-        $input = $request->query->get("input");
+        $input = $request->query->get("denomination");
         $input = htmlspecialchars(preg_replace("/[^a-zA-Z0-9 ]+/", "", Strip_tags($input)));
         // dd($input);
 
@@ -38,17 +39,53 @@ class RechercheController extends AbstractController
           $response = $callApiService->getDataEntrepriseList($input);
         }catch(Exception $e)
         {
-          
+
           $response = null;
         }
       }
 
           return $this->render('recherche/index.html.twig', [
-            'controller_name' => 'RechercheController',
             'entrepriseList' => $response,
             'inputText' => $input,
         ]);
 
+    }
+
+
+    #[Route('/{slug}', name: 'byslug')]
+    public function slug($slug, Request $request, CallApiService $callApiService): Response
+    {
+        $response = null;
+      //  $input = $request->query->get("input");
+        $input = htmlspecialchars(preg_replace("/[^a-zA-Z0-9 ]+/", "", Strip_tags($slug)));
+        // dd($input);
+
+      if (is_numeric($input) && strlen($input) == 9 || strlen($input) == 14 )
+      {
+        try {
+
+          $response = $callApiService->getSirenData($input);
+          return $this->redirectToRoute("entrepriseSearch", array("slug"=>$input ));
+
+        } catch (\Exception $e) {
+
+        }
+
+      }else
+      {
+        try{
+          $response = $callApiService->getDataEntrepriseList($input);
+        }catch(Exception $e)
+        {
+
+          $response = null;
+        }
+      }
+
+          return $this->render('recherche/index.html.twig', [
+            'entrepriseList' => $response,
+            'inputText' => $input,
+        ]);
 
     }
 }
